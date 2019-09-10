c57e428de4c65eec845e7705bbce3b26887f0161
caoyongqi <v-caoyongqi@xiaomi.com>
2019-09-10 17:34:43 +0800

    add requests

--- Api/urls.py
+++ Api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from Api.views import Api_test
+from Api.views import Api_test, Api_send
 
-app_name='Api'
+app_name = 'Api'
 urlpatterns = [
-    path('api/',Api_test.as_view(),name='api')
+    path('requests/', Api_test.as_view(), name='requests'),
+    path('send/', Api_send.as_view(), name='send')
 ]
--- Api/views.py
+++ Api/views.py
@@ -1,3 +1,7 @@
+import json
+
+import requests
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,3 +11,52 @@ from django.views.generic.base import View
 class Api_test(View):
     def get(self, request):
         return render(request, 'apply/Api/Api.html')
+
+
+class Api_send(View):
+    def post(self, request):
+        url = request.POST.get('url', '')
+        method = request.POST.get("method", '')
+        res = RequestPool().requestTool(method, url)
+        if res:
+            res = json.dumps(json.loads(res.text), indent=4)
+            data = {
+                'status': 'ok',
+                'res': res
+            }
+            return JsonResponse(data)
+        else:
+            data = {
+                'status': 'err'
+            }
+            return JsonResponse(data)
+
+
+class RequestPool:
+    def __init__(self):
+        self.res = None
+
+    def requestTool(self, method, url, headers=None):
+
+        if method == 'GET':
+            try:
+                self.res = requests.get(url=url, headers=headers)
+            except Exception:
+                return False
+        elif method == "POST":
+            try:
+                self.res = requests.post(url=url, headers=headers)
+            except Exception as e:
+                return False
+        elif method == "PUT":
+            try:
+                self.res = requests.put(url=url, headers=headers)
+            except Exception as e:
+                return False
+        else:
+            try:
+                self.res = requests.delete(url=url, headers=headers)
+            except Exception as e:
+                return False
+
+        return self.res
